777066b1202bc6e1af34bc0a786e7ba8c5fd03b5
twahyono <teddy@qnptech.com>
2025-10-08 21:34:55 +0700

    refactor: download file using fs/promises

--- routes/v1/file/index.js
+++ routes/v1/file/index.js
@@ -1,5 +1,6 @@
 import { pipeline } from "node:stream/promises";
-import fs from "node:fs/promises";
+import fsPromise from "node:fs/promises";
+import fs from "node:fs";
 import path from "node:path";
 import { v1 as uuidv1 } from "uuid";
 import { fileURLToPath } from "url";
@@ -126,26 +127,55 @@ async function routes(fastify, opts) {
     }
   );
 
-  fastify.get("/streams", async function (request, reply) {
-    await new Promise((res) => setTimeout(res, 100));
-    const stream = fs.createReadStream(
-      path.join(__dirname, "../../../upload/sample.pdf")
-    );
-    reply.header("Content-Type", "application/octet-stream");
-    reply.send(stream);
-  });
-
-  fastify.get("/buffer", async function (request, reply) {
-    try {
-      await new Promise((res) => setTimeout(res, 100));
-      const fileBuffer = await fs.readFile(
-        path.join(__dirname, "../../../upload/sample.pdf")
-      );
-      reply.send(fileBuffer);
-    } catch (err) {
-      reply.send(err);
+  fastify.get(
+    "/streams/:filename",
+    {
+      schema: {
+        params: {
+          type: "object",
+          properties: { filename: { type: "string" } },
+        },
+      },
+    },
+    async function (request, reply) {
+      try {
+        const filename = request.params.filename;
+        await new Promise((res) => setTimeout(res, 100));
+        const stream = fs.createReadStream(
+          path.join(__dirname, "../../../upload/" + filename)
+        );
+        reply.header("Content-Type", "application/octet-stream");
+        return reply.send(stream);
+      } catch (e) {
+        fastify.log.error(e);
+        throw e;
+      }
     }
-  });
+  );
+
+  fastify.get(
+    "/buffer/:filename",
+    {
+      schema: {
+        params: {
+          type: "object",
+          properties: { filename: { type: "string" } },
+        },
+      },
+    },
+    async function (request, reply) {
+      try {
+        const filename = request.params.filename;
+        await new Promise((res) => setTimeout(res, 100));
+        const fileBuffer = await fsPromise.readFile(
+          path.join(__dirname, "../../../upload/" + filename)
+        );
+        reply.send(fileBuffer);
+      } catch (err) {
+        reply.send(err);
+      }
+    }
+  );
 }
 
 export default routes;
