11641047816a99c9d37a265bb635a813d03da90f
teddy wahyono <teddy@qnptech.com>
2025-11-29 10:57:38 +0700

    fix: remove shadow_database_url, not use for pg

--- prisma.config.js
+++ prisma.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
   },
   datasource: {
     url: env("DATABASE_URL"),
-    shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+    //shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
   },
 });
